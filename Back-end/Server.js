ce98b250255af16ab861210d12b0ea17c4cb35ca
alaeddinebara <baraalaeddine@gmail.com>
2024-10-17 16:23:17 +0100

    fff

--- Back-end/Server.js
+++ Back-end/Server.js
@@ -24,7 +24,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Middleware --
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
