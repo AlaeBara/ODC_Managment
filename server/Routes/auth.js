b26fc877dfeca15aae3aea684e49e20183bc9e06
abdennacer kaddouri <135185867+mazzinno@users.noreply.github.com>
2024-10-18 12:43:51 +0100

    Update auth.js

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -6,15 +6,6 @@ const router = express.Router();
 
 router.post('/signIn', Login);
 
-router.get('/validate-token', authenticated, (req, res) => {
-    res.status(200).json({ message: 'Token is valid.', user: req.user });
-});
-
-router.get('/verifier_admin', authenticated, (req, res) => {
-  res.status(200).json({ message: 'Welcome to the admin dashboard' , user: req.user });
-});
-
-
 router.get('/logout', (req, res) => {
   res.clearCookie('token');
   res.status(200).json({ message: 'Logged out successfully' });
